e1ad8744a61f416cde9773913265e676adb74621
WolfWood72 <elaric@ngs.ru>
2017-09-19 00:35:17 +0700

    0,99
    Вычисления проводятся правильно,критерии считаются правильно.выводится
    график

--- PIAA_lab1.py
+++ PIAA_lab1.py
@@ -1,19 +1,56 @@
 import numpy as np
 from  IO import *
 from model import *
+import matplotlib.pyplot as plt
 plan_list = IO.input_plans_from_file('input.txt')
 pp = [model(plan) for plan in plan_list ]
 
+print('№\t\tD\t\tA\t\tE\t\tF2\t\tLyambda\t\tMV\t\tG')
+for i in range(len(pp)):
+    print('{}\t{:08.4f}\t{:08.4f}\t{:08.4f}\t{:08.4f}\t{:08.4f}\t{:08.4f}\t{:08.4f}\t{}'.format(i,
+                                                 pp[i].crit_D,
+                                                 pp[i].crit_A,
+                                                 pp[i].crit_E,
+                                                 pp[i].crit_Ф,
+                                                 pp[i].crit_Lymbda,
+                                                 pp[i].crit_MV,
+                                                 pp[i].crit_G,
+                                                 pp[i].sum_crit))
 
-graphic_info = []
-for q in np.arange(0.01,0.99,0.01):
-    if q != 0.5:
+plot_x = [0]
+plot_y = [0]
+for q in np.arange(0,0.51,0.01):
+    if q not in (0.5,-0.5,0):
         plan = np.array([elem_plan(-1, q),
                             elem_plan(-0.5, (1 - 2 * q) / 2),
                             elem_plan(0.5, (1 - 2 * q) / 2),
                             elem_plan(1, q)])
-        D_crit = model(plan).Criterium.D
-        graphic_info.append((q,D_crit))
+        D_crit = model(plan).crit_D
+        #graphic_info.append((q,D_crit))
+        plot_x.append(q)
+        plot_y.append(D_crit)
 a = 1
 
+print("q")
+print((plot_y.index(max(plot_y)) + 1) * 0.01)
+print("value")
+print(max(plot_y))
+
+
+fig = plt.figure()
+plt.plot(plot_x, plot_y)
+
+plt.scatter((plot_y.index(max(plot_y)) + 1) * 0.01, max(plot_y))
+plt.title('D - optimality')
+plt.ylabel('Criterion value')
+plt.xlabel('q')
+plt.grid(True)
+plt.text((plot_y.index(max(plot_y)) + 1) * 0.01 + 0.01, max(plot_y), 'max', fontsize=12)
+#plt.scatter((result.index(max(result)) + 1) * 0.01, max(result))
+#plt.title('D - optimality')
+#plt.ylabel('Criterion value')
+#plt.xlabel('q')
+#plt.grid(True)
+#plt.text((result.index(max(result)) + 1) * 0.01 + 0.01, max(result), 'max', fontsize=12)
+plt.show()
 
--- model.py
+++ model.py
@@ -10,14 +10,14 @@ class elem_plan(object):
 
 class model(object):
     """description of class"""
-    class Criterium(object):
+    '''class Criterium(object):
         D = None
         A = None
         E = None
         Ф = None
         Lymbda = None
         MV = None
-        G = None
+        G = None'''
             
 
 
@@ -42,13 +42,24 @@ class model(object):
         self.D = np.linalg.inv(self.M)
 
     def Calc_Criterium(self):
-        self.Criterium.D = self.Calc_D()
-        self.Criterium.A = self.Calc_A()
-        self.Criterium.E = self.Calc_E()
-        self.Criterium.Ф = self.Calc_Ф()
-        self.Criterium.Lymbda = self.Calc_Lymabda()
-        self.Criterium.MV = self.Calc_MV()
-        self.Criterium.G = self.Calc_G()
+       #self.Criterium.D = self.Calc_D()
+       #self.Criterium.A = self.Calc_A()
+       #self.Criterium.E = self.Calc_E()
+       #self.Criterium.Ф = self.Calc_Ф()
+       #self.Criterium.Lymbda = self.Calc_Lymabda()
+       #self.Criterium.MV = self.Calc_MV()
+       #self.Criterium.G = self.Calc_G()
+       self.crit_D = self.Calc_D()
+       self.crit_A = self.Calc_A()
+       self.crit_E = self.Calc_E()
+       self.crit_Ф = self.Calc_Ф()
+       self.crit_Lymbda = self.Calc_Lymabda()
+       self.crit_MV = self.Calc_MV()
+       self.crit_G = self.Calc_G()
+       self.sum_crit = self.crit_D + self.crit_A + self.crit_E + self.crit_Ф + self.crit_Lymbda+   self.crit_MV + self.crit_G
+
+
+
 
     def Calc_D(self):
         return  np.linalg.det(self.M)
@@ -60,7 +71,7 @@ class model(object):
     def Calc_Ф(self, p=2):
         return ((1.0 / self.m)* (self.D**p).trace())** (1.0/p)
     def Calc_Lymabda(self):
-        return np.sum(np.linalg.eig(self.D)[0] - np.average(np.linalg.eig(self.D)[0])) ** 2
+        return np.sum((np.linalg.eig(self.D)[0] - np.average(np.linalg.eig(self.D)[0]))**2 )
     def Calc_MV(self):
         return np.max(np.diag(self.D))
     def Calc_G(self):
@@ -73,39 +84,3 @@ class model(object):
 
 
 
-'''
-class OptimaCriterium(object):
-    def __init__(self,plan_list,m,fx):
-        self.plan_list = plan_list  
-        self.cnt_plan = len(plan_list)  
-        self.m = m
-        self.fx = fx
-
-    def Calc_D(self):
-        return np.max([np.linalg.det(self.plan_list[i].M) for i in range(self.cnt_plan)])
-    def Calc_E(self):
-        return np.min([np.max(np.linalg.eig(plan_list[i].D)[0]) for i in range(self.cnt_plan)])
-        
-    def Calc_A():
-        return np.min([plan_list[i].D.trace() for i in range(self.cnt_plan)])
-    #Допилить
-    def Calc_Ф(self):
-        return np.min([(1.0 / self.m) * plan_list[i].D.trace() for i in range(self.cnt_plan)]) ** (1.0 / 2.0)
-    def Calc_Lymabda(self):
-        return np.min([np.sum(np.linalg.eig(plan_list[i].D)[0] - np.average(np.linalg.eig(plan_list[i].D)[0])) ** 2 for i in  range(self.cnt_plan)])
-    def Calc_MV(self):
-        return np.min([np.max(np.diag(plan_list[i].D)) for i in range(self.cnt_plan)])
-    def Calc_G(self):
-        def d(x,plan):
-            return self.fx(x).T @ plan.D @ self.fx
-       
-        for i in range(self.cnt_plan):
-            for j in self.plan_list[i].plan:
-                d(j.x,self.plan_list[i].plan)
-'''
-
-
-    
-
-
-    
