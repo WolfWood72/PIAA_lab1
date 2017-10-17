03d3a6ee45dce87d9e025d02b9d8518dfed72de6
WolfWood72 <elaric@ngs.ru>
2017-10-17 20:14:05 +0700

    1.5
    Ничего не работает все плохо

--- PIAA_lab2.py
+++ PIAA_lab2.py
@@ -4,8 +4,63 @@ from model import *
 import matplotlib.pyplot as plt
 
 
+def Optimize(plan):
+    pass
+
+def Gradient(Plan):
+    D = plan.D
+    gradient = []
+    for i in plan.plan:
+        f = plan.f(i.x)
+        gradient.append(f.T@plan.D@f)
+    return gradient
+
+def Make_A(q,n):
+    Aq = np.zeros((q + 1,n)).T
+    Aq[n-1] = 1        
+    for i in range(q+1):
+        Aq[i][i] = -1
+    return np.eye(n,n) - Aq.T@np.linalg.inv(Aq@Aq.T)@Aq
+
 x = [-1 , -0.5 ,0 , 0.5 , 1]
-plan = IO.auto_plan(1/25,x)
-pp = model(plan)
-pp.optimize()
-a = 1
+
+plan = model(IO.auto_plan(1/25,x))
+isCont = True
+L = 0.001
+iter = 1
+while isCont:
+    isCont = False
+    q = 0
+    n = plan.n_plan
+    for i in plan.plan:
+        if i.p <= 1e-5:
+            q = q+1
+        else:
+            break
+    grad = Gradient(plan)
+    t_phi =  grad[q+1]
+    for k in grad[q+1:]:
+        if t_phi != k:
+            isCont = True
+
+    avg = sum(grad[q+1:])/(n-q)
+    for k in range(0,q+1):
+        if avg - grad[k] < 0:
+            isCont = True
+    
+
+    Gq = grad - avg
+    for k in range(q):
+        Gq[k] *= -1
+    plan_s = []
+    if isCont:
+        #Gg = Make_A(q,n)@Gq
+        for k in range(n):
+            plan_s.append(elem_plan(plan.plan[k].x,plan.plan[k].p+L*Gq[k]))
+        a = 0
+        plan = model(plan_s)
+        print('n_iter {}'.format(iter))
+        print(plan_s)
+        print('\n\n')
+        iter += 1
+a = 0
--- model.py
+++ model.py
@@ -19,8 +19,6 @@ class model(object):
         self.Make_M()
         self.Make_D()
         self.Calc_Criterium()
-    '''def f(self,x):
-        return np.array([1,x,x * x])'''
 
     def f(self,x):
         return np.array([1,x[0],x[1],x[0] * x[1],x[0] * x[0],x[1] * x[1]])
@@ -43,75 +41,8 @@ class model(object):
        self.crit_Lymbda = self.Calc_Lymabda()
        self.crit_MV = self.Calc_MV()
        self.crit_G = self.Calc_G()
-       self.sum_crit = self.crit_D + self.crit_A + self.crit_E + self.crit_Ф + self.crit_Lymbda + self.crit_MV + self.crit_G
+       self.sum_crit = self.crit_D + self.crit_A + self.crit_E + self.crit_Ф + self.crit_Lymbda+   self.crit_MV + self.crit_G
 
-        
-    
-    def Gradient(self,plan):
-        Grad = []
-        cur_plan = model(plan)
-        for k in range(len(plan)):
-            f = self.f(self.plan[k].x)
-            M_xk = np.zeros((self.m,self.m))
-            '''for i in range(self.m):
-                    M_xk[i] +=  f[i] * f * plan[k].p'''
-            Grad.append(f.T@cur_plan.D@f)
-
-        return np.array(Grad)
-
-    def MakeG(self,n,q):
-        '''Aq = np.zeros((q + 1,n)).T
-        Aq[n-1] = 1        
-        for i in range(q+1):
-            Aq[i][i] = -1
-        tt = np.linalg.det(Aq@Aq.T)
-        if tt == 0:
-            return np.eye(n,n)
-        else:
-            return np.eye(n,n) - Aq.T@np.linalg.inv(Aq@Aq.T)@Aq
-        '''
-        pass
-
-
-    def phi(self,p_x,p_ksi):
-        x = model(p_x)
-        ksi = model(p_ksi)
-        return (ksi.D@x.M).trace()
-    def NextPlan(self,plan,lyambda,gradient,n,q):
-        G = self.MakeG(n,q)
-        ps_1 = plan
-        tt = lyambda*G@gradient
-        for i in range(n):
-            ps_1[i].p = plan[i].p + tt[i]
-        return ps_1
-
-    def optimize(self):
-        plan = self.plan
-        n = self.n_plan
-        IcContinue = True
-        while IcContinue:
-            IcContinue = True
-            q = len([1 for i in self.plan if np.abs(i.p) <= 1e-5 ])
-            grad = self.Gradient(plan)
-            
-            s_fi = np.sum(grad[q+1:])/(n-q)
-            Gp_Grad = np.zeros(n)
-            for j in range(q+1,n):
-                Gp_Grad[j] = grad[j] - s_fi
-            flag = False
-            for i in range(q + 1,n):
-                for j in range(q + 1,n):
-                    if grad[i] != grad[j]:
-                        flag = True                            
-            if not flag:
-                IcContinue = False
-            for j in range(q):
-                if s_fi - grad[j] <= 0 :
-                    IcContinue = True
-            if  IcContinue:
-                for i in range(n):
-                    plan[i].p = plan[i].p + 1* Gp_Grad[i]
-        return plan
 
 
 
@@ -122,20 +53,12 @@ class model(object):
     def Calc_A(self):
         return self.D.trace()
     def Calc_Ф(self, p=2):
-        return ((1.0 / self.m) * (self.D ** p).trace()) ** (1.0 / p)
+        return ((1.0 / self.m)* (self.D**p).trace())** (1.0/p)
     def Calc_Lymabda(self):
-        return np.sum((np.linalg.eig(self.D)[0] - np.average(np.linalg.eig(self.D)[0])) ** 2)
+        return np.sum((np.linalg.eig(self.D)[0] - np.average(np.linalg.eig(self.D)[0]))**2 )
     def Calc_MV(self):
         return np.max(np.diag(self.D))
     def Calc_G(self):
         def d(x):
             return self.f(x).T @ self.D @ self.f(x)
         return np.max([ d(self.plan[i].x) for i in range(self.n_plan)])
-
-
-
-
-
-
-
-
